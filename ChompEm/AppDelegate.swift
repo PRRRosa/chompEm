5ac810afaf8c964f193f91164b65590ca285a36f
leafarmd <leafarmd@gmail.com>
2015-07-08 12:59:49 -0300

    melhorias na jogabilidade e notificações

--- ChompEm/AppDelegate.swift
+++ ChompEm/AppDelegate.swift
@@ -23,13 +23,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         var notification = UILocalNotification()
         notification.alertBody = "I'm hungry!"
         notification.alertAction = "open"
-        notification.fireDate = NSDate().dateByAddingTimeInterval(10)
+        notification.fireDate = NSDate().dateByAddingTimeInterval(2*7*24*3600)
         notification.soundName = UILocalNotificationDefaultSoundName
-        notification.userInfo = ["title": "Reminder Test"]
+        notification.userInfo = ["title": "Reminder"]
         notification.category = "TODO_CATEGORY"
         
-        UIApplication.sharedApplication().scheduleLocalNotification(notification)
         
+        if let UIApplication.sharedApplication().scheduledLocalNotifications.first{
+            UIApplication.sharedApplication().scheduledLocalNotifications.first?.scheduleLocalNotification(notification)
+        }
+        else{
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        }
         
         return true
     }
